6544ff4b4d84909c3c9aa5f6137187a0c316d952
Sarah Allen <sarah@ultrasaurus.com>
2011-03-20 23:17:31 -0700

    new app with person scaffold and rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClassApp::Application.config.secret_token = '9408ed8d0cdf512cb37c2e4e3c974d14f3e6a29b6bf9afcc9f326648d465ba0ac865092fe8582727071a9baef441c1443c72036c17932516dc0fb35b310be08c'
