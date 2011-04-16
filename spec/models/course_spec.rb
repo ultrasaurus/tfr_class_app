f0f9c4dae5c2a648ee637e7175ad0dcbf6d7722a
Sarah Allen <sarah@ultrasaurus.com>
2011-04-16 06:26:33 -0700

    added rspec and gen'd course spec

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,5 @@
-require 'spec_helper'
+require 'spec_helper'  #<label id="code.spec_helper"/> 
 
-describe Course do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Course do  #<label id="code.course_describe"/> 
+  pending "add some examples to (or delete) #{__FILE__}"  #<label id="code.course_spec_default_pending"/> 
 end
