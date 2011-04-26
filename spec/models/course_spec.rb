d0296aee81e6a64763d03426c43819901b8a16ad
Sarah Allen <sarah@ultrasaurus.com>
2011-04-26 04:30:11 -0700

    fix line break

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Course do  
   it 'should save a valid record when created with title and description' do
-    c = Course.create!(:title => "Creative Writing", :description => "Learn to write fiction!") 
+    c = Course.create!(:title => "Creative Writing", 
+                       :description => "Learn to write fiction!") 
   end
 end
