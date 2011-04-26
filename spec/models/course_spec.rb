371041dd2991b5317e4deb4b95751428300d36de
Sarah Allen <sarah@ultrasaurus.com>
2011-04-26 06:27:02 -0700

    removed experiment

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -5,4 +5,10 @@ describe Course do
     c = Course.create!(:title => "Creative Writing", 
                        :description => "Learn to write fiction!") 
   end
+# START:title_validation
+  it "should not be valid if it has no title" do
+    c = Course.new(:description => "Learn to write fiction!") 
+    c.should_not be_valid
+  end
+# END:title_validation
 end
