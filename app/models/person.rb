2c93830b96d7da3f7e94173676e751da6a41e826
Sarah Allen <sarah@ultrasaurus.com>
2011-04-30 08:03:19 -0700

    added full_name method

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,10 @@
 class Person < ActiveRecord::Base
-  #validates_presence_of :given_name
+  validates_presence_of :given_name
+
+# START:method
+  def full_name
+    "#{given_name} #{surname}"
+  end
+# END:method
+  
 end
